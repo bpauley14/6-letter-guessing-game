acaf8d1e6548a18fea03680d11868e1550562a1d
Braedan Pauley <braedanpauley@gmail.com>
2024-03-06 12:08:50 -0500

    dmca fixes

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,7 +9,7 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source word guessing game based on Wordle
+        This is an open source 6 letter word guessing game
       </p>
     </BaseModal>
   )
